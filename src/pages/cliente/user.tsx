e03018862cf51d78dd90f4d28bb3c1dc6c15139d
miguel1man <miguelangelhh17@gmail.com>
2022-10-28 22:48:18 -0500

    update client design

--- src/pages/cliente/user.tsx
+++ src/pages/cliente/user.tsx
@@ -36,14 +36,14 @@ const User: NextPage = () => {
     <Layout title="Home Page">
       <div className="flex flex-col items-center ">
         <div className="flex w-full justify-center mt-5 mb-5">
-          <div>
-            <p className="font-bold text-xl">CAJERO: ADOLFO LLANOS</p>
-            <p className="text-xs text-center">
+          <div >
+            <p className="text-left font-bold text-xl text-neutral-800">CAJERO: ADOLFO LLANOS</p>
+            <p className="text-xs text-left text-neutral-800">
               ID: {blockchain.accountCajero}
             </p>
           </div>
         </div>
-        <button className="flex items-center border-2 border-primary text-primary font-semibold p-2 rounded-md hover:bg-primary hover:text-white shadow-[0_8px_30px_rgb(0,0,0,0.12)]">
+        <button className="flex items-center border-2 border-primary text-primary font-semibold p-2 rounded-md hover:text-neutral-800 hover:border-neutral-800">
           <figure className="pr-2">
             <Image
               src="/images/ion_qr-code-sharp.svg"
@@ -54,18 +54,18 @@ const User: NextPage = () => {
           </figure>
           Escanear Cliente
         </button>
-        <div className="w-full flex justify-center pt-5 mt-5">
+        <div className="w-96 flex justify-center pt-5 mt-5">
           <div
-            className={`px-2 text-md font-medium ${
-              tabActive === "grant" ? "text-primary" : ""
+            className={`mx-2 px-2 py-3 text-md border-b-2 border-neutral-800 text-neutral-800 font-medium ${
+              tabActive === "grant" ? "text-primary border-[#43CBC3]" : ""
             }`}
             onClick={() => setTabActive("grant")}
           >
             OTORGAR PUNTOS
           </div>
           <div
-            className={`px-2 text-md font-medium ${
-              tabActive === "reward" ? "text-primary" : ""
+            className={`mx-2 px-2 py-3 text-md border-b-2 border-neutral-800 text-neutral-800 font-medium ${
+              tabActive === "reward" ? "text-primary border-[#43CBC3]" : ""
             }`}
             onClick={() => setTabActive("reward")}
           >
@@ -74,7 +74,7 @@ const User: NextPage = () => {
         </div>
         {tabActive === "grant" && (
           <div>
-            <div className="flex flex-col items-center pt-11 pb-6 rounded-lg mt-10 mb-20 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+            <div className="flex flex-col items-center pt-11 pb-6 rounded-3xl mt-10 mb-12 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
               <figure>
                 <Image
                   src="/images/Bronze-CertCard.png"
@@ -104,7 +104,7 @@ const User: NextPage = () => {
                 {/* <p className="font-semibold text-lg">{data.tokensCajero} Pts</p> */}
               </div>
             </div>
-            <div className="flex flex-col items-center pt-11 rounded-lg mt-10 mb-20 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+            <div className="flex flex-col items-center pt-11 rounded-3xl mt-10 mb-12 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
               <div className="flex items-center">
                 {/* <p className="font-normal text-sm text-black/50">
                   Hasta 23/10/2022
@@ -151,7 +151,7 @@ const User: NextPage = () => {
                   </div>
 
                   <button
-                    className="flex items-center bg-primary text-white font-semibold px-1 rounded-md h-10 mt-1"
+                    className="flex items-center bg-primary text-white font-semibold px-3 rounded-md h-10 mt-1"
                     disabled={false}
                     onClick={handleButtonTx}
                   >
@@ -189,7 +189,8 @@ const User: NextPage = () => {
         )}
         {tabActive === "reward" && (
           <>
-            <div className="flex flex-col items-center pt-11 ">
+          <div className="w-96">
+            <div className="flex flex-col items-center pt-11 pb-10 rounded-3xl mt-10 mb-12 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
               <figure>
                 <Image
                   src="/images/Bronze-CertCard.png"
@@ -206,128 +207,131 @@ const User: NextPage = () => {
               <p className="font-bold text-base mt-4">Fecha de alta</p>
               <p className="font-normal text-sm">10 Mayo 2022</p>
             </div>
-            <div className="w-full flex justify-center pt-5">
-              <div
-                className={`px-2 text-md font-medium ${
-                  tabActiveClient === "exchange" ? "text-primary" : ""
-                }`}
-                onClick={() => settabActiveClient("exchange")}
-              >
-                CANJEAR PUNTOS
+            <div className="flex flex-col items-center pt-11 pb-10 rounded-3xl mt-10 mb-12 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+              <div className="w-full flex justify-center pt-5">
+                <div
+                  className={`px-2 text-md font-medium ${
+                    tabActiveClient === "exchange" ? "text-primary" : ""
+                  }`}
+                  onClick={() => settabActiveClient("exchange")}
+                >
+                  CANJEAR PUNTOS
+                </div>
+                <div
+                  className={`px-2 text-md font-medium ${
+                    tabActiveClient === "promotion" ? "text-primary" : ""
+                  }`}
+                  onClick={() => settabActiveClient("promotion")}
+                >
+                  PROMOCIONES
+                </div>
               </div>
-              <div
-                className={`px-2 text-md font-medium ${
-                  tabActiveClient === "promotion" ? "text-primary" : ""
-                }`}
-                onClick={() => settabActiveClient("promotion")}
-              >
-                PROMOCIONES
-              </div>
-            </div>
-            {tabActiveClient === "exchange" && (
-              <div className="flex flex-col items-center font-bold text-lg mt-10">
-                <p className="px-14 text-center">
-                  Canjeo de la compra del cliente en puntos
-                </p>
-                <div className="h-px w-72 bg-black/10 mt-10" />
-                <ul className="w-80">
-                  <li className="flex  justify-between">
-                    <div className="flex">
-                      <figure>
-                        <Image
-                          src={"/images/Medals-gold.svg"}
-                          width={60}
-                          height={60}
-                          alt="icon point"
-                        />
-                      </figure>
-                      <div>
-                        <p className="font-semibold">Aceite primor</p>
-                        <p className="font-normal text-xs">S/12.00</p>
+              {tabActiveClient === "exchange" && (
+                <div className="flex flex-col items-center font-bold text-lg mt-10">
+                  <p className="px-14 text-center">
+                    Canjeo de la compra del cliente en puntos
+                  </p>
+                  <div className="h-px w-72 bg-black/10 mt-10" />
+                  <ul className="w-80">
+                    <li className="flex  justify-between">
+                      <div className="flex">
+                        <figure>
+                          <Image
+                            src={"/images/Medals-gold.svg"}
+                            width={60}
+                            height={60}
+                            alt="icon point"
+                          />
+                        </figure>
+                        <div>
+                          <p className="font-semibold">Aceite primor</p>
+                          <p className="font-normal text-xs">S/12.00</p>
+                        </div>
                       </div>
-                    </div>
-                    <p>12 ptos</p>
-                  </li>
-                  <li className="flex justify-between">
-                    <div className="flex">
-                      <figure>
-                        <Image
-                          src={"/images/Medals-gold.svg"}
-                          width={60}
-                          height={60}
-                          alt="icon point"
-                        />
-                      </figure>
-                      <div>
-                        <p className="font-semibold">Paneton Donofrio</p>
-                        <p className="font-normal text-xs">S/36.00</p>
+                      <p>12 ptos</p>
+                    </li>
+                    <li className="flex justify-between">
+                      <div className="flex">
+                        <figure>
+                          <Image
+                            src={"/images/Medals-gold.svg"}
+                            width={60}
+                            height={60}
+                            alt="icon point"
+                          />
+                        </figure>
+                        <div>
+                          <p className="font-semibold">Paneton Donofrio</p>
+                          <p className="font-normal text-xs">S/36.00</p>
+                        </div>
                       </div>
-                    </div>
-                    <p>36 ptos</p>
-                  </li>
-                  <li className="flex justify-between">
-                    <div className="flex">
-                      <figure>
-                        <Image
-                          src={"/images/Medals-gold.svg"}
-                          width={60}
-                          height={60}
-                          alt="icon point"
-                        />
-                      </figure>
-                      <div>
-                        <p className="font-semibold">Mancuerna</p>
-                        <p className="font-normal text-xs">S/56.00</p>
+                      <p>36 ptos</p>
+                    </li>
+                    <li className="flex justify-between">
+                      <div className="flex">
+                        <figure>
+                          <Image
+                            src={"/images/Medals-gold.svg"}
+                            width={60}
+                            height={60}
+                            alt="icon point"
+                          />
+                        </figure>
+                        <div>
+                          <p className="font-semibold">Mancuerna</p>
+                          <p className="font-normal text-xs">S/56.00</p>
+                        </div>
                       </div>
-                    </div>
-                    <p>56 ptos</p>
-                  </li>
-                </ul>
+                      <p>56 ptos</p>
+                    </li>
+                  </ul>
 
-                <div className="flex justify-between w-10/12 bg-black/10 px-4 py-4  rounded-3xl">
-                  <div>
-                    <p className="font-semibold text-normal">Canjealo por</p>
-                    <p className="font-normal text-sm text-black/60">
-                      Corresponde a s/104.00
+                  <div className="flex justify-between w-10/12 bg-black/10 px-4 py-4  rounded-3xl">
+                    <div>
+                      <p className="font-semibold text-normal">Canjealo por</p>
+                      <p className="font-normal text-sm text-black/60">
+                        Corresponde a s/104.00
+                      </p>
+                    </div>
+                    <p className="font-semibold text-normal">104 Ptos</p>
+                  </div>
+                  <button className="flex items-center bg-primary text-white font-semibold px-7 py-3 rounded-md my-10">
+                    Canjear puntos
+                  </button>
+                  <div className="flex flex-col items-center">
+                    <figure>
+                      <Image
+                        src="/images/sending-message.svg"
+                        width={150}
+                        height={126}
+                        alt="icon loading"
+                      />
+                    </figure>
+                    <p className="text-base font-semibold text-center">
+                      !Enhorabuena!
+                    </p>
+                    <p className="px-10 pt-3 pb-10 text-center font-normal">
+                      Fueron gastados <b>120 puntos</b> de la billetera de Gustavo
                     </p>
                   </div>
-                  <p className="font-semibold text-normal">104 Ptos</p>
                 </div>
-                <button className="flex items-center bg-primary text-white font-semibold px-7 py-3 rounded-md my-10">
-                  Canjear puntos
-                </button>
-                <div className="flex flex-col items-center">
-                  <figure>
-                    <Image
-                      src="/images/sending-message.svg"
-                      width={150}
-                      height={126}
-                      alt="icon loading"
-                    />
-                  </figure>
-                  <p className="text-base font-semibold text-center">
-                    !Enhorabuena!
+              )}
+              {tabActiveClient === "promotion" && (
+                <div className="flex flex-col items-center font-bold text-lg mt-10">
+                  <p className="px-14 text-center text-2xl">
+                    Promociones vigentes
                   </p>
-                  <p className="px-10 pt-3 pb-10 text-center font-normal">
-                    Fueron gastados <b>120 puntos</b> de la billetera de Gustavo
+                  <p className="font-normal text-center px-16 mt-5 mb-10">
+                    <b>Gustavo</b> por ser cliente Oro, llévate este pack
+                    canjeandolo con <b>500 Ptos</b>
                   </p>
+                  {/* <div>
+                    <Image />
+                  </div> */}
                 </div>
-              </div>
-            )}
-            {tabActiveClient === "promotion" && (
-              <div className="flex flex-col items-center font-bold text-lg mt-10">
-                <p className="px-14 text-center text-2xl">
-                  Promociones vigentes
-                </p>
-                <p className="font-normal text-center px-16 mt-5 mb-10">
-                  <b>Gustavo</b> por ser cliente Oro, llévate este pack
-                  canjeandolo con <b>500 Ptos</b>
-                </p>
-                {/* <div>
-                  <Image />
-                </div> */}
-              </div>
-            )}
+              )}
+            </div>
+          </div>
           </>
         )}
       </div>
